d400c5da371942ba599ddf8a7614a480e5b8735b
Tamal Dey <tdey@mcftech.com>
2018-09-06 12:08:02 +0530

    initial commit

---
+++ app/User.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use DesignMyNight\Mongodb\Auth\User as Model;
+
+
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Foundation\Auth\Access\Authorizable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Laravel\Passport\HasApiTokens;
+
+
+class User extends Model implements AuthenticatableContract, AuthorizableContract,CanResetPasswordContract
+{
+    use Authenticatable, Authorizable, CanResetPassword, HasApiTokens, Notifiable;
+
+    protected $connection = 'mongodb';
+    protected $collection = 'users';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
